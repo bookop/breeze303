2c7ee43daffa07880eff85d0cc3a5b0f3696a04f
bookop <44890140+bookop@users.noreply.github.com>
2025-03-21 20:44:15 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -77,6 +77,9 @@ git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mo
 # Alist
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
+#OAF
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter 
+
 # DDNS.to
 git_sparse_clone main https://github.com/linkease/nas-packages-luci luci/luci-app-ddnsto
 git_sparse_clone master https://github.com/linkease/nas-packages network/services/ddnsto
